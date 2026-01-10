ea14a10fe3020695e3365d66b307bc6b2eb21f17
Vishal Choudhary <vishalchoudhary0968@gmail.com>
2026-01-10 17:50:51 +0530

    first commit

---
+++ src/components/Contact/contact.js
@@ -0,0 +1,49 @@
+import './contact.css';
+
+import facebookIcon from '../../assets/facebook-icon.png';
+import twitterIcon from '../../assets/twitter.png';
+import youtubeIcon from '../../assets/youtube.png';
+import instagramIcon from '../../assets/instagram.png';
+import React, { useRef } from 'react';
+import emailjs from '@emailjs/browser';
+
+const Contact = () => {
+    const form = useRef();
+
+    const sendEmail = (e) => {
+        e.preventDefault();
+
+        emailjs.sendForm('service_6phm4ar', 'template_zcxds5a', form.current, 'blaHt_5KIJ711DfGH')
+            .then((result) => {
+                console.log(result.text);
+                e.target.reset();
+                alert('Email Sent !');
+            }, (error) => {
+                console.log(error.text);
+            });
+    };
+    
+    return (
+        <div id='contactPage'>
+
+            <div id="contact">
+                <h1 className="contactPageTitle">Contact Me</h1>
+                <span className="contactDesc">Please fill out the form below to discuss any work opportunities.</span>
+                <form className="contactForm" ref={form} onSubmit={sendEmail}>
+                    <input type="text" className="name" placeholder='Your name' name='from_name' />
+                    <input type="text" className="email" placeholder='Your Email' name='from_email' />
+                    <textarea name="message" placeholder='Your Message' rows={5} className='msg' ></textarea>
+                    <button type="submit" value="Send" className='submitBtn'>Submit</button>
+                    <div className="links">
+                        <img src={facebookIcon} alt="Facebook" className="link" />
+                        <img src={twitterIcon} alt="Twitter" className="link" />
+                        <img src={youtubeIcon} alt="YouTube" className="link" />
+                        <img src={instagramIcon} alt="Instagram" className="link" />
+                    </div>
+                </form>
+            </div>
+        </div>
+    );
+}
+
+export default Contact;
---
+++ src/components/Skills/skills.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import './skills.css';
+import UIDesign from '../../assets/ui-design.png';
+import WebDesign from '../../assets/website-design.png';
+
+const Skills = () => {
+    return (
+        <section id='skills'>
+            <span className="skillTitle">What I do</span>
+            <span className="skillDesc">I am a skilled and passionate web designer with experience in creating visually appealing and user-friendly websites. I have a strong understanding of design and a keen eye for detail. I am proficient in HTML, CSS, JavaScript, React as well as design software such as Adobe Photoshop and Illustrator.</span>
+            <div className="skillBars">
+                <div className="skillBar">
+                    <img src={UIDesign} alt="UIDesign" className="skillBarImg" />
+                    <div className="skillBarText">
+                        <h2>UI/UX Design</h2>
+                        <p>This is a demo text, you can write your own content here.</p>
+                    </div>
+                </div>
+                <div className="skillBar">
+                    <img src={WebDesign} alt="WebDesign" className="skillBarImg" />
+                    <div className="skillBarText">
+                        <h2>Website Design</h2>
+                        <p>This demo text can be changed while making the production ready website.</p>
+                    </div>
+                
+               
+                </div>
+            </div>
+        </section>
+    );
+}
+
+export default Skills;
